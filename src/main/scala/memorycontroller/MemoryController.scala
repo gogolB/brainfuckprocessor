68574071789da7654bef6a6ab28e40dda778df83
Gogol Bhattacharya <gogol.bhattacharya@kla-tencor.com>
2020-10-26 11:51:50 -0700

    Updated Memory Controller

--- src/main/scala/memorycontroller/MemoryController.scala
+++ src/main/scala/memorycontroller/MemoryController.scala
@@ -12,7 +12,7 @@ import chisel3.util._
  */
 class MemoryController (n:Int) extends Module {
   val io = IO(new Bundle {
-    val ADDR         = Input(UInt(log2Ceil(n).W))
+    val ADDR            = Input(UInt(log2Ceil(n).W))
 
     val WR_DATA         = Input(UInt(32.W))
     val RD_DATA         = Output(UInt(32.W))
@@ -20,26 +20,51 @@ class MemoryController (n:Int) extends Module {
     val NUM_WORDS       = Input(UInt(2.W))
 
     val WR              = Input(Bool())
+    val RD              = Input(Bool())
 
-    val isValid         = Output(Bool())
+    val isDone          = Output(Bool())
   })
 
   val mem = new RAM(n)
 
-    
-    when(io.WR) {
-        // We are in read mode.
-        io.isValid = false
+  object State extends ChiselEnum {
+    val sIdle, sRead, sReadDone, sWrite, sWriteDone= Value
+  }
+  
+  val state = RegInit(State.sNone)
 
+  isDone := false.B
+  switch (state) {
 
-        io.isValid = true
-
-    }.otherwise {
-        // We are in read mode.
-        io.isValid = false
-
-        io.isValid = true
+    // Idle State
+    is (State.sIdle) {
+      when (io.WR) {
+        state := sWrite
+      }.elsewhen(io.RD) {
+        state := sRead
+      }.otherwise {
+        state := sIdle
+      }
     }
 
+    // PrereadState
+    is (State.sRead) {
+      isDone := false.B
+    }
 
+    // Finished Read State
+    is (State.sReadDone) {
+      isDone := true.B
+    }
+
+    // PreWriteState
+    is (State.sWrite) {
+      isDone := false.B
+    }
+
+    // Finished Write State
+    is (State.sWriteDone ) {
+      isDone := true.B
+    }
+  }
 }
